00ad0058bffe52ba82eac5b966a09f9c09c2e39c
gcanu <guy.canu@gmail.com>
2021-03-10 23:26:54 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,81 @@
-import {
-  AfterContentInit,
-  Component,
-  ContentChildren,
-  Directive,
-Host,
-HostBinding,
-  QueryList,
-  VERSION,
-  ViewChild
-} from "@angular/core";
+import {AfterContentInit, Component, ContentChildren, Directive, ElementRef, HostBinding, QueryList, VERSION} from "@angular/core";
 
-export const Position = {
-  GAUCHE: 'gauche',
-  DROITE: 'droite'
-} as const;
+export enum Position {
+    GAUCHE,
+    DROITE
+}
 
-export type Position = typeof Position[keyof typeof Position];
-
+export const Positions = new Map<Position, string>([
+    [Position.GAUCHE, 'gauche'],
+    [Position.DROITE, 'droite']
+]);
 
 @Directive({
-  selector: "[tl-etiquette]"
+    selector: "[tl-etiquette]"
 })
 export class TlEtiquette {
-  position: string;
+    position: string;
 
-  @HostBinding('class') get gauche():string {return this.position};
+    constructor(
+        private readonly el: ElementRef
+    ) {
+    }
 
-  setPosition(position: Position) {
-    this.position = position;
-  }
+    @HostBinding('class') get gauche(): string {
+        return this.position
+    };
+
+    setPosition(position: Position) {
+        this.position = Positions.get(position);
+    }
+
+    getElement(): HTMLElement {
+        return this.el.nativeElement;
+    }
 }
 
 @Directive({
-  selector: "[tl-container]"
+    selector: "[tl-container]"
 })
 export class TlContainer implements AfterContentInit {
-  @ContentChildren(TlEtiquette, {descendants: true}) etiquettes!: QueryList<TlEtiquette>;
+    @ContentChildren(TlEtiquette, {descendants: true}) etiquettes!: QueryList<TlEtiquette>;
 
-  ngAfterContentInit() {
-    this.etiquettes.forEach((etiquette: TlEtiquette) => {
-      const i: number = Math.floor(Math.random() * 2);
-      /*const p = Object.keys(Position)[i];
-      etiquette.setPosition(Position[]);*/
-    })
-  }
+    constructor(
+        private readonly container: ElementRef
+    ) {
+    }
+
+    ngAfterContentInit() {
+        const etiquette = this.etiquettes
+          .reduce((e: TlEtiquette, etiquette: TlEtiquette) => {
+              const tailleMax = e.getElement().clientWidth;
+              const taille = etiquette.getElement().clientWidth;
+
+              console.log(tailleMax, taille);
+
+
+              return taille > tailleMax ? etiquette : e;
+            }, this.etiquettes.first
+          );
+
+
+        const containerEl = this.container.nativeElement as HTMLElement;
+        let element: HTMLElement = etiquette.getElement()
+        
+        while(element && element !== containerEl) {
+            element = element.parentElement;
+        }
+
+        console.log('here');
+        etiquette.setPosition(Position.GAUCHE);
+    }
 }
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+    selector: "my-app",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "Angular " + VERSION.major;
+    name = "Angular " + VERSION.major;
 }
